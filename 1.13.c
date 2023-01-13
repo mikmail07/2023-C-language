0310ba17bd221d12821916641d9b88eb7aee308e
mikmail07 <122246184+mikmail07@users.noreply.github.com>
2023-01-13 13:11:18 +0900

    Add files via upload

---
+++ 1.13.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main()
+{ 
+    char c;
+    scanf("%c",&c);
+    printf("%d", ('A' <= c) && (c <= 'Z') || ('a' <= c)&&( c<='z')?1:0);
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
