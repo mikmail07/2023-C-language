fa1ea949d6f12781c5176cecb800564f61f7bb8b
mikmail07 <122246184+mikmail07@users.noreply.github.com>
2023-01-18 12:33:31 +0900

    1.18.2023

---
+++ 1.18수.c
@@ -0,0 +1,33 @@
+
+# include<stdio.h>
+int main()
+{
+
+    int n, result;
+    scanf("%d", &n);
+    
+
+        if(n<0)printf("cold,indoor\n");
+        if(n>=0&&n<40) printf("moderate,outdoor\n");
+        if(n>=40) printf("hot, indoor\n");
+
+
+
+
+       
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
